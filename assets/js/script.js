a529739acbf29e941d4c94c509ebf94c7839cb7f
Mona <forward2cm@gmail.com>
2019-07-04 21:41:51 -0700

    flipped one card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+$(document).ready(initializeApp);
 
+function handleCardClick(event){
+console.log(event);
+
+   var a= (event.currentTarget.nextElementSibling.className);
+   console.log(a);
+
+
+   $(event.currentTarget).toggleClass(a);
+
+
+    
+}
+
+function initializeApp(){
+    $(".first").click(handleCardClick);
+    $(".second").click(handleCardClick);
+}
